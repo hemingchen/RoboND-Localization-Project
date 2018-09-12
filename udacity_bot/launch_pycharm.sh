7d6bb556380fe4b5e1434aca7ee6b8693586dc09
hemingchenhc@gmail.com <hemingchenhc@gmail.com>
2018-09-12 09:44:21 -0400

    first completed version, using udacity defined robot.

--- udacity_bot/launch_pycharm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Activate the ROS environment and then launch pycharm
-
-PYCHARM_DIR=pycharm-community-2018
-source /home/robond/catkin_ws/devel/setup.bash && /opt/${PYCHARM_DIR}/bin/pycharm.sh &
